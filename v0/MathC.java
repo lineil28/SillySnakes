3a77e28c5fb41835e6d4364206659791a1d67758
Vansh <vsaboo30@stuy.edu>
2022-01-15 00:21:37 -0500

    Fixed permutation, added choose, added some comments to explain

--- v0/MathC.java
+++ v0/MathC.java
@@ -1,5 +1,7 @@
 public class MathC{
 
+    // Probability Functions
+    
     static int factorial(int num){
       if(num < 0){throw new RuntimeException("Invalid input - less than 0");}
       if(num == 0){return 1;}
@@ -9,12 +11,30 @@ public class MathC{
     }
 
     static int permute(int n, int r){
+      // nPr = n! / (n - r)!
+
       if(n < r){throw new RuntimeException("Invalid input - r is greater than n");}
-      if(n < 0 || n < r){throw new RuntimeException("Invalid input - n < 0 or r < 0"); }
+      if(n < 0 || r < 0){throw new RuntimeException("Invalid input - n < 0 or r < 0"); }
+
+      // return factorial(n) / factorial(n - r)
+
       int product = 1;
-      for(int val = n; val > n - r; val--){product *= val; }
+      for(int val = n; val > n - r; val--){product *= val; } // Calculates first r terms in n! sequence
       return product;
      }
+
+    static int choose(int n, int r){
+        // nCr = n! / r!(n - r)!
+        // return factorial(n) / ( factorial(n - r) * factorial(r)  )
+
+        /* nCr = n! / r!(n - r)!
+        = n! / (n - r)!   * 1 / r!
+        = nPr/r!
+        */
+        return permute(n, r) / factorial(r);
+     }
+
+
     // Trig functions Suite
 
     // Reciprocal Trig Functions
