a0010b7e32fcab02c7552c43a480ae0a4e7d6e6a
Vansh <vsaboo30@stuy.edu>
2022-01-18 23:29:59 -0500

    Added square root simplifier, factorizer, and user interface for trigonometry

--- v1/MathC.java
+++ v1/MathC.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class MathC{
     // Generic Math methods
 
@@ -19,7 +20,42 @@ public class MathC{
         return(a * b / gcd(a, b));
     }
 
+    // Simplify a square root
+    static String simplifySqrt(int num){
+      String output = "", i = "";
+      int val = num, maxSquareFactor = 1;
 
-    
+      if(val == 0){return "0";}
+
+      if (val < 0) {
+        i += "i";
+        val *= -1;
+      }
+
+      for(int j = 2; j <= val; j++){
+        while(val % (j * j) == 0){
+          maxSquareFactor *= j;
+          val /= (j * j);
+        }
+      }
+
+      if(maxSquareFactor != 1) output += maxSquareFactor + ""; // "1" is redundant inside and outside of the square root
+      output += i + ""; // Add "i" if imaginary
+      if(val != 1) output +=  "\u221A" + val;
+
+      return output;
+    }
+
+    // Return the factors of a number
+    static ArrayList<Integer> factors(int num){
+      int val = Math.abs(num);
+      ArrayList<Integer> factorList = new ArrayList<Integer>(1);
+
+      for(int i = 1; i <= val; i++){
+        if(num % i == 0){factorList.add(i);}
+      }
+      if(factorList.size() == 0){factorList.add(0); }
+      return factorList;
+    }
 
 }
