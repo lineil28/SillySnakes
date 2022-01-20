0b34d6efb4664d8c3fdd970734290d65612e1ffc
Vansh Saboo <vsaboo30@stuy.edu>
2022-01-20 14:09:08 -0500

    Quadratic solver update

--- v3/QuadraticSolver.java
+++ v3/QuadraticSolver.java
@@ -3,7 +3,8 @@ public class QuadraticSolver{
     String output = "The roots of (" + a + ")x^2 + (" + b + ")x + (" + c + ") = 0 are ";
 
       int discriminant = b * b - 4 * a * c;
-      if(discriminant == 0 || Math.sqrt(discriminant) * Math.sqrt(discriminant) == discriminant){
+      String midTerm = MathC.simplifySqrt(discriminant);
+      if(midTerm.indexOf("i") == -1 && midTerm.indexOf("\u221A") == -1){
         int plusMinus = Integer.parseInt(MathC.simplifySqrt(discriminant));
         Rational rootOne = new Rational(-b + plusMinus, 2 * a);
         Rational rootTwo = new Rational(-b - plusMinus, 2 * a);
@@ -12,9 +13,13 @@ public class QuadraticSolver{
         output += rootOne.toString() + ", " + rootTwo.toString();
       }
       else{
-        String rootOne = "(" + (-b) + " + (" + MathC.simplifySqrt(b * b - 4 * a * c) + ") )/" + (2 * a);
-        String rootTwo = "(" + (-b) + " - (" + MathC.simplifySqrt(b * b - 4 * a * c) + ") )/" + (2 * a);
-          output += rootOne + ", " + rootTwo;
+        int partition = 0;
+        while(partition < midTerm.length() && midTerm.substring(partition, partition + 1) != "i" && midTerm.substring(partition, partition + 1) != "\u221A"){
+          partition += 1;
+        }
+        int maxSquareFactor = Integer.parseInt( midTerm.substring(0, partition) );
+        String disc = midTerm.substring(partition, midTerm.length() );
+        System.out.println(maxSquareFactor + " " + disc);
       }
       return output;
 
